7817e97c0cb39b61b83a5ebf81e4bb2f511e146c
z19521257 <OVlaBuzmakova@sberbank.ru>
2022-01-28 22:30:44 +0300

    add MoviesCard adaptive

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './MoviesCardList.css';
 import MoviesCard from "../MoviesCard/MoviesCard";
+import film from "../../images/film.jpg"
 
-function MoviesCardList(props) {
+function MoviesCardList() {
     return (
         <div className="content">
             <div className="content__elements">
-                <MoviesCard name="33 слова о дизайне" duration="1ч 47м"/>
-                <MoviesCard name="33 слова о дизайне" duration="1ч 47м"/>
-                <MoviesCard name="33 слова о дизайне" duration="1ч 47м"/>
-                <MoviesCard name="33 слова о дизайне" duration="1ч 47м"/>
+                <MoviesCard name="33 слова о дизайне" duration="1ч 47м" image={film}/>
+                <MoviesCard name="33 слова о дизайне" duration="1ч 47м" image={film}/>
+                <MoviesCard name="33 слова о дизайне" duration="1ч 47м" image={film}/>
+                <MoviesCard name="33 слова о дизайне" duration="1ч 47м" image={film}/>
             </div>
             <button className="content-more__button">Ещё</button>
         </div>
