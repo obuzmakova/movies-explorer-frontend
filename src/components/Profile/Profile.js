cb465fc69a1b10cfcd973c9031c0562b7c082d63
z19521257 <OVlaBuzmakova@sberbank.ru>
2022-01-30 23:06:42 +0300

    add Profile for all res and actions

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,15 +6,25 @@ function Profile(props) {
     const [data, setData] = useState({
         name: 'Ольга',
         email: 'olga@yandex.ru',
-        disable: true,
-        buttonText: 'Редактировать',
-        buttonExitText: 'Выйти из аккаунта'
+        disable: true
     })
 
     function handleEdit(e) {
         e.preventDefault();
 
+        setData({
+            ...data,
+            ['disable']: false
+        })
+    }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setData({
+            ...data,
+            ['disable']: true
+        })
     }
 
     return (
@@ -22,7 +32,7 @@ function Profile(props) {
             <p className="profile__welcome">
                 Привет, {data.name}!
             </p>
-            <form onSubmit={handleEdit}>
+            <form onSubmit={handleSubmit}>
                 <div className="profile__rows">
                     <div className="profile__row">
                         <label htmlFor="name">
@@ -37,13 +47,16 @@ function Profile(props) {
                             E-mail
                         </label>
                         <input id="email" name="email" type="email" className="profile__text"
-                               value={data.email} disabled={false}/>
+                               value={data.email} disabled={data.disable}/>
                     </div>
                 </div>
-                <button type="submit" className="profile__button">{data.buttonText}</button>
+                {data.disable ? <button type="submit" className="profile__button profile__button-submit"
+                                        onClick={handleEdit}>Редактировать</button> :
+                    <button type="submit" className="profile__button profile__button-save" onClick={handleSubmit}>Сохранить</button>
+                }
             </form>
 
-            <Link to="/signin" className="profile__exit-link">Выйти из аккаунта</Link>
+            {data.disable ? <Link to="/signin" className="profile__button profile__button-exit">Выйти из аккаунта</Link> : null}
         </div>
     );
 }
