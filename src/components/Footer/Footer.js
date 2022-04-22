fcd2b9393b06d334f2318997dcdaf79f8711f6c3
z19521257 <OVlaBuzmakova@sberbank.ru>
2022-04-22 22:10:07 +0300

    fix year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
             </div>
             <div className="footer__about">
-                <p className="footer__copyright">© 2021</p>
+                <p className="footer__copyright">© 2022</p>
                 <div className="footer__links">
                     <a className="footer__link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
                     <a className="footer__link" href="https://github.com/obuzmakova" target="_blank" rel="noreferrer">Github</a>
